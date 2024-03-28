01432b00915961e1396eccc8bdac3ac3912de5df
Nataliia Smalchenko <yarokrilka@gmail.com>
2024-03-28 16:55:28 +0200

    path and routes fix

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,6 @@
 import { Link, Route, Routes } from 'react-router-dom';
-import { Suspense, lazy } from 'react';
+import { lazy } from 'react';
 import Header from './Header/Header';
-import Loader from './Loader/Loader';
 
 import MovieDetailsPage from '../pages/MovieDetailsPage/MovieDetailsPage';
 import CastPage from '../pages/CastPage/CastPage';
@@ -13,59 +12,25 @@ const HomePage = lazy(() => import('../pages/HomePage/HomePage'));
 const App = () => {
   return (
     <>
-      <Header />
       <Routes>
-        <Route
-          path="/"
-          element={
-            <Suspense fallback={<Loader />}>
-              <HomePage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/movies"
-          element={
-            <Suspense fallback={<Loader />}>
-              <MoviesPage />
-            </Suspense>
-          }
-        />
-        <Route
-          path="/movies/:movieId"
-          element={
-            <Suspense fallback={<Loader />}>
-              <MovieDetailsPage />
-            </Suspense>
-          }
-        >
+        <Route path="/" element={<Header />}>
+          <Route index element={<HomePage />} />
+          <Route path="movies" element={<MoviesPage />} />
+          <Route path="movies/:movieId" element={<MovieDetailsPage />}>
+            <Route path="cast" element={<CastPage />} />
+            <Route path="reviews" element={<ReviewsPage />} />
+          </Route>
           <Route
-            path="cast"
+            path="*"
             element={
-              <Suspense fallback={<Loader />}>
-                <CastPage />
-              </Suspense>
-            }
-          />
-          <Route
-            path="reviews"
-            element={
-              <Suspense fallback={<Loader />}>
-                <ReviewsPage />
-              </Suspense>
+              <section className="container">
+                <Link className="go-to-home" to="/">
+                  Go to home page
+                </Link>
+              </section>
             }
           />
         </Route>
-        <Route
-          path="*"
-          element={
-            <section className="container">
-              <Link className="go-to-home" to="/">
-                Go to home page
-              </Link>
-            </section>
-          }
-        />
       </Routes>
     </>
   );
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,25 +1,32 @@
-import { NavLink } from 'react-router-dom';
+import { NavLink, Outlet } from 'react-router-dom';
+import { Suspense } from 'react';
 
 import css from './Header.module.css';
+import Loader from 'components/Loader/Loader';
 
 const Header = () => {
   return (
-    <header className={css.header}>
-      <nav>
-        <ul className={css['nav-list']}>
-          <li>
-            <NavLink className={css.link} to="/">
-              Home
-            </NavLink>
-          </li>
-          <li>
-            <NavLink className={css.link} to="/movies">
-              Movies
-            </NavLink>
-          </li>
-        </ul>
-      </nav>
-    </header>
+    <>
+      <header className={css.header}>
+        <nav>
+          <ul className={css['nav-list']}>
+            <li>
+              <NavLink className={css.link} to="/">
+                Home
+              </NavLink>
+            </li>
+            <li>
+              <NavLink className={css.link} to="/movies">
+                Movies
+              </NavLink>
+            </li>
+          </ul>
+        </nav>
+      </header>
+      <Suspense fallback={<Loader />}>
+        <Outlet />
+      </Suspense>
+    </>
   );
 };
 
